831cee218586ff56e6b81e71cd230b45eaa340f9
Samrit Basnet <samritbasnet70@gmail.com>
2025-03-28 12:43:59 -0400

    final changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,13 @@
 import { FiBookmark, FiSearch, FiUser } from "react-icons/fi";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import image from '../../assets/logo/image.png';
 import "./Header.scss";
-
 const Header = () => {
-    const location = useLocation();
-
     return (
         <nav className="header">
 
             <NavLink to="/" className="header__logo">
-                <img src="src/assets/logo/image.png" alt="Max Logo" className="header__logo-img" />
+                <img src={image} alt="Max Logo" className="header__logo-img" />
             </NavLink>
 
             <div className="header__left">
